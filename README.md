5ede5fdfbb4c0cf37daf3a1d94d03e464002fde1
Kevin <junho2413@gmail.com>
2024-11-11 14:01:26 -0500

    test

---
+++ README.md
@@ -0,0 +1,12 @@
+# Kevin's TIL
+
+  I've learned 1 things in 2 categories so far!
+
+  | Date       | Title | Description |
+  |------------|-------|-------------|
+  
+# TEST
+
+
+| 2024-11-11 | [test title](TEST\test.md) |  |
+  
---
+++ generate-readme.js
@@ -0,0 +1,122 @@
+const fs = require("fs");
+const util = require("util");
+const path = require("path");
+
+const readdir = util.promisify(fs.readdir);
+
+const excludedFolders = [".git"];
+const excludedFiles = [
+  "generate-readme.js",
+  "README.md",
+  "package-lock.json",
+  "package.json",
+];
+
+async function countFilesAndFolders(dir) {
+  const files = await readdir(dir, { withFileTypes: true });
+
+  let fileCount = 0;
+  let folderCount = 0;
+
+  for (const file of files) {
+    if (excludedFolders.includes(file.name)) {
+      continue;
+    }
+
+    if (file.isDirectory()) {
+      folderCount++;
+      const subDir = path.join(dir, file.name);
+      const subCounts = await countFilesAndFolders(subDir);
+      fileCount += subCounts.fileCount;
+      folderCount += subCounts.folderCount;
+    } else {
+      if (excludedFiles.includes(file.name)) {
+        continue;
+      }
+      fileCount++;
+    }
+  }
+
+  return { fileCount, folderCount };
+}
+
+async function countFolders(dir) {
+  const files = await readdir(dir, { withFileTypes: true });
+  let folderCount = 1;
+
+  for (const file of files) {
+    if (file.isDirectory() && !excludedFolders.includes(file.name)) {
+      folderCount++;
+    }
+  }
+
+  return folderCount;
+}
+
+async function getFileDate(filePath) {
+  const stats = await fs.promises.stat(filePath);
+  return stats.mtime.toISOString().split("T")[0];
+}
+
+async function generateTable(dir, depth = 0) {
+  const files = await readdir(dir, { withFileTypes: true });
+
+  let tableContent = "";
+
+  const title = depth === 0 ? "#" : "#".repeat(depth + 2);
+
+  for (const file of files) {
+    if (file.isDirectory()) {
+      if (excludedFolders.includes(file.name)) {
+        continue;
+      }
+      const subDir = path.join(dir, file.name);
+      const subTable = await generateTable(subDir, depth + 1);
+
+      tableContent += `
+${title} ${file.name}
+
+${subTable}`;
+    } else {
+      if (excludedFiles.includes(file.name)) {
+        continue;
+      }
+      const filePath = path.join(dir, file.name);
+      const fileContent = await fs.promises.readFile(filePath, {
+        encoding: "utf-8",
+      });
+      const lines = fileContent.trim().split("\n");
+
+      const firstLine = lines[0].replace(/^#+\s*/, "");
+      const secondLine = lines.length > 1 ? lines[1] : "";
+
+      const fileDate = await getFileDate(filePath);
+
+      tableContent += `
+| ${fileDate} | [${firstLine}](${path.relative(
+        __dirname,
+        filePath
+      )}) | ${secondLine} |`;
+    }
+  }
+
+  return tableContent;
+}
+
+async function generateReadme() {
+  const counts = await countFilesAndFolders(__dirname);
+  const folderCounts = await countFolders(__dirname);
+  const tableContent = await generateTable(__dirname);
+  const readmeContent = `# Kevin's TIL
+
+  I've learned ${counts.fileCount} things in ${folderCounts} categories so far!
+
+  | Date       | Title | Description |
+  |------------|-------|-------------|
+  ${tableContent}
+  `;
+
+  fs.writeFileSync("README.md", readmeContent);
+}
+
+generateReadme().catch((error) => console.error(error));
